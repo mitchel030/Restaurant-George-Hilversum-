18a1448b36061db55e6160a358c8cecccc678eec
mitchel030 <72606177+mitchel030@users.noreply.github.com>
2021-09-27 16:08:50 +0200

    Resevering + login + resvering

---
+++ Login/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Reservation Form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        
+        <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Hello, world!</title>
+  </head>
+
+    <body>
+        <!-- Navbar-->
+
+        <!-- Header-->
+        <header class="py-5" style="background-color: rgb(255, 255, 255);">
+            
+            <div class="container px-4 px-lg-5">
+                <img src="./img/logogeorgemarina.png" class="rounded mx-auto d-block" alt="...">
+            </div>
+            <hr/>
+        </header>
