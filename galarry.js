ef7a2f692ce1e25e6ee44c119caf88861b7e856d
mitchel030 <72606177+mitchel030@users.noreply.github.com>
2021-11-29 15:42:36 +0100

    Login+register+logout

---
+++ galarry.js
@@ -0,0 +1,12 @@
+function test() {
+    alert("Hoi");
+    var p = document.getElementById("exampleFormControlSelect1");
+    alert(p.value);
+    var text = document.getElementById("rratest");
+    text.innerHTML = p.value;
+    alert("Hoi");
+    var p = document.getElementById("exampleFormControlSelect2");
+    alert(p.value);
+    var text = document.getElementById("text");
+    text.innerHTML = p.value;
+}
